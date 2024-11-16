ffc08e8d2b44229ae257f3e74f9a6f6928c2ce93
Mageche10 <mageche10@gmail.com>
2024-11-16 11:49:34 +0100

    Product data and Attribute data analysis

---
+++ dataset_analysis.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+# For each attribute, which values are possibilities?
+attribute_data = pd.read_csv('./data/attribute_data.csv')
+
+possible = attribute_data.groupby("attribute_name")["des_value"].unique()
+
+print(possible.reset_index())
+
+# Relations between product category and families/type
+
+product_data = pd.read_csv('./data/product_data.csv')
+
+relations = product_data.groupby("des_product_category")["des_product_type"].unique()
+
+print(relations.reset_index())
+
+
+

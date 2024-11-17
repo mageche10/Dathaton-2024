afdb2c516e4d625f0adbff3885b48b7d2b1a4d73
TheSpaceHub <danvancea1235813@gmail.com>
2024-11-17 05:29:34 +0100

    really really good update

--- very_updated_handle_output.py
+++ very_updated_handle_output.py
@@ -27,9 +27,20 @@ test_df[predicting_columns] = test_df[predicting_columns].astype("category")
 test_df[predicting_columns] = test_df[predicting_columns].apply(lambda x: x.cat.codes)
 
 for attr in attributes:
-    pred = []
+    attributes_set = pd.read_csv('./data/attribute_data.csv')
+    attributes_names = attributes_set["attribute_name"].unique()
+
+    all_values = attributes_set[attributes_set["attribute_name"] == attr][['cod_value', 'des_value']]
+    non_duplicated = all_values.drop_duplicates(subset=['cod_value']).sort_values(by=['cod_value'])
+
+    values_list = non_duplicated['des_value'].tolist()
+    predictions = []
+    image_names = []
+
     count = 0
     for img in images:
+        if count > 100:
+            break
         current_df = test_df[test_df["des_filename"] == img]
         current_df = current_df.head(1)[predicting_columns]
         current_df = current_df
@@ -40,8 +51,21 @@ for attr in attributes:
         np_img = zoom(np_img, (1/red_factor, 1/red_factor))
         np_img = np.divide(np_img, 255)
         img_array = np_img[np.newaxis, ..., np.newaxis]
-        pred.append(np.argmax((models[attr]).predict([img_array, current_df], verbose=0) , axis=1))
+
+        predictions.append(np.argmax((models[attr]).predict([img_array, current_df], verbose=0) , axis=1)[0])
+        image_names.append(img)
+
         count += 1
         if count % 100 == 0:
             print(count, " / ", len(images))
-    pred.to_csv("./predictions/very_updated_" + attr + ".csv", index = False)
+    
+    submission_dataframe = pd.DataFrame(columns=["test_id", "des_value"])
+    j = 0
+    for img_name, pred_label in zip(image_names, predictions):
+        #print(f"Image: {img_name} - Predicted Label: {pred_label}")
+        pattern = r"^\d+_\d+"
+
+        submission_dataframe.loc[j] = [re.match(pattern, img_name).group() + '_' + attr,
+                                  values_list[pred_label - 1] if pred_label != 0 else 'INVALID']
+        j = j + 1
+    submission_dataframe.to_csv("./predictions/very_updated_" + attr + ".csv", index = False)
